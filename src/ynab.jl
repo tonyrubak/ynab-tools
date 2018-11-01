2e4cf37f757b089ab1c217b548790c8ec968fb87
Joe <joe@crimsonhexagon.us>
2018-10-31 20:48:05 -0400

    added more information to reconciliation report

--- src/ynab.jl
+++ src/ynab.jl
@@ -56,42 +56,48 @@ function get_account_transactions(api, budget_id, account_id)
 end
 
 function reconcile(transactions, reconcile_date, stmt_balance,
-                   start_date, open_balance)
+                   open_date, open_balance)
+
     reconcile_data = filter(tx -> parse_date(tx["date"]) <= reconcile_date &&
-                            parse_date(tx["date"]) >= start_date,
+                            parse_date(tx["date"]) >= open_date,
                             transactions)
     cleared_tx = filter(tx -> tx["cleared"] == "cleared", reconcile_data)
     uncleared_tx = filter(tx -> tx["cleared"] != "cleared", reconcile_data)
     
     cleared_balance = open_balance
     uncleared_balance = 0
+
+    println("*** Reconciliation Report ***")
+    println("Period Ending: $reconcile_date")
+    println()
+    println("Open       - \$ $(format_acct(open_balance))")
+    println("*** Cleared Transactions ***")
     
     for tx in cleared_tx
         cleared_balance += tx["amount"]
+        println("$(tx["date"]) - \$ $(format_acct(tx["amount"]))")
     end
-
+    
     if length(uncleared_tx) > 0
+        println()
         println("*** Uncleared transactions ***")
         for tx in uncleared_tx
-            println("$(tx["date"]) - $(tx["amount"])")
+            println("$(tx["date"]) - \$ $(format_acct(tx["amount"]))")
             uncleared_balance += tx["amount"]
         end
     end
 
-    if cleared_balance != stmt_balance
-        diff = cleared_balance - stmt_balance
-        println("*** Balance Discrepancy ***")
-        println("YNAB balance: $cleared_balance")
-        println("Statement balance: $stmt_balance")
-        println("Difference (cleared-statement): $diff")
-    else
-        println("*** Balances Match ***")
+    diff = stmt_balance - cleared_balance
+    println()
+    println("YNAB balance: \$ $(format_acct(cleared_balance))")
+    if (diff != 0)
+        println("reconciliation discrepancy: \$ $(format_acct(diff))")
     end
+    println("Statement balance: \$ $(format_acct(stmt_balance))")
+    
     Dict("cleared" => cleared_balance,
          "uncleared" => uncleared_balance,
-         "diff" => diff,
          "cleared_tx" => cleared_tx,
-         "uncleared_tx" => uncleared_tx,
-         "end_dt" => reconcile_date)
+         "uncleared_tx" => uncleared_tx)
 end
 end
